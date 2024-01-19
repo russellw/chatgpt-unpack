98fb9a9c0c46f6ab4849ff178e41e3b9aa1e5bf9
Russell Wallace <russell.wallace@gmail.com>
2024-01-19 17:01:21 +0000

    headings

--- chatgpt-unpack.go
+++ chatgpt-unpack.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 import "github.com/buger/jsonparser"
@@ -14,29 +15,35 @@ func main() {
 		log.Fatal(err)
 	}
 	_, err = jsonparser.ArrayEach(text, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		// Access fields of each conversation
-		title, _ := jsonparser.GetString(value, "title")
-		createTime, _ := jsonparser.GetFloat(value, "create_time")
-		updateTime, _ := jsonparser.GetFloat(value, "update_time")
+		if err != nil {
+			log.Fatal(err)
+		}
+		title, err := jsonparser.GetString(value, "title")
+		if err != nil {
+			log.Fatal(err)
+		}
+		updateTime, err := jsonparser.GetFloat(value, "update_time")
+		if err != nil {
+			log.Fatal(err)
+		}
+		tm := time.Unix(int64(updateTime), 0)
+		fmt.Printf("%s:: %s\n", tm.Format("2006-01-02 15:04:05 MST"), title)
+		/*
+			// Access the 'mapping' object within the conversation
+			mapping, _, _, _ := jsonparser.Get(value, "mapping")
+			// Iterate over the mapping objects
+			jsonparser.ObjectEach(mapping, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+				// Access fields of each mapping object
+				mappingID := string(key)
+				parent, _ := jsonparser.GetString(value, "parent")
 
-		fmt.Printf("Title: %s\n", title)
-		fmt.Printf("Create Time: %.6f\n", createTime)
-		fmt.Printf("Update Time: %.6f\n", updateTime)
+				fmt.Printf("Mapping ID: %s\n", mappingID)
+				fmt.Printf("Parent: %s\n", parent)
 
-		// Access the 'mapping' object within the conversation
-		mapping, _, _, _ := jsonparser.Get(value, "mapping")
-		// Iterate over the mapping objects
-		jsonparser.ObjectEach(mapping, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
-			// Access fields of each mapping object
-			mappingID := string(key)
-			parent, _ := jsonparser.GetString(value, "parent")
-
-			fmt.Printf("Mapping ID: %s\n", mappingID)
-			fmt.Printf("Parent: %s\n", parent)
-
-			return nil
-		}, "mapping")
-	}, "conversations")
+				return nil
+			}, "mapping")
+		*/
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
